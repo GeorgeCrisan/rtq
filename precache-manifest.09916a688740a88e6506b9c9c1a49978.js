77b473d8b3e89ad3663d5897e4123964a17de5f1
George Crisan <georgerdp@gmail.com>
2020-07-18 12:43:17 +0100

    Updates

---
+++ precache-manifest.09916a688740a88e6506b9c9c1a49978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf35b2f02f3736af60386adf8e0134e",
+    "url": "/rtq/index.html"
+  },
+  {
+    "revision": "41e4669a01bab728a33e",
+    "url": "/rtq/static/js/2.71877584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rtq/static/js/2.71877584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a5f0b55d07faf71a23",
+    "url": "/rtq/static/js/main.c7d6eefd.chunk.js"
+  },
+  {
+    "revision": "a439d8afc5f524c560f4",
+    "url": "/rtq/static/js/runtime-main.80e61ea2.js"
+  },
+  {
+    "revision": "713539fe809b194edbc0c7f21967a64f",
+    "url": "/rtq/static/media/fog.713539fe.jpg"
+  }
+]);
